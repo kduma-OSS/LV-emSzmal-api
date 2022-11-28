5c461a3de6039190612d3e0da7560db28b6273fe
Krystian Duma <git@krystian.duma.sh>
2022-11-29 00:10:59 +0100

    Update laravel service provider

--- src/Laravel/ServiceProvider.php
+++ src/Laravel/ServiceProvider.php
@@ -29,19 +29,24 @@ class ServiceProvider extends LaravelServiceProvider
         });
 
         $this->app->singleton(emSzmalAPI::class, function (Application $app) {
-            $api = new emSzmalAPI(config('emszmalapi.license.api_id'), config('emszmalapi.license.api_key'), config('emszmalapi.timeout', 120));
-            $api->setCacheProvider($app->make(CacheProviderInterface::class));
+            $api = new emSzmalAPI(
+                api_id: config('emszmalapi.license.api_id'),
+                api_key: config('emszmalapi.license.api_key'),
+                timeout: config('emszmalapi.timeout', 120),
+                cache_provider: $app->make(CacheProviderInterface::class),
+            );
+            
             $api->setDefaultBankCredentialsResolver(function ($identifier = 'default') {
                 if (! config('emszmalapi.bank_credentials.'.$identifier)) {
                     throw new Exception('There is no credentials with id '.$identifier.'!');
                 }
 
                 return new BankCredentials(
-                    config('emszmalapi.bank_credentials.'.$identifier.'.provider') ?? '',
-                    config('emszmalapi.bank_credentials.'.$identifier.'.login') ?? '',
-                    config('emszmalapi.bank_credentials.'.$identifier.'.password') ?? '',
-                    config('emszmalapi.bank_credentials.'.$identifier.'.user_context') ?? '',
-                    config('emszmalapi.bank_credentials.'.$identifier.'.token_value') ?? ''
+                    provider: config('emszmalapi.bank_credentials.'.$identifier.'.provider') ?? '',
+                    login: config('emszmalapi.bank_credentials.'.$identifier.'.login') ?? '',
+                    password: config('emszmalapi.bank_credentials.'.$identifier.'.password') ?? '',
+                    user_context: config('emszmalapi.bank_credentials.'.$identifier.'.user_context') ?? '',
+                    token_value: config('emszmalapi.bank_credentials.'.$identifier.'.token_value') ?? ''
                 );
             });
 
